64e50c22513cbcdfea4b050d1eddaf121ad74478
Akumuuu <88854566+Akumuuu@users.noreply.github.com>
2023-11-28 17:43:03 +0100

    змінював js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,6 @@
 //Slider
-document.addEventListener('DOMContentLoaded', function () {
-  const slider = document.querySelector('.slider');
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slider");
   const radioButtons = document.querySelectorAll(
     '.radio-buttons input[type="radio"]'
   );
@@ -8,22 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
   let intervalId;
 
   radioButtons.forEach(function (radio, index) {
-    radio.addEventListener('change', function () {
+    radio.addEventListener("change", function () {
       changeSlide(index);
       clearInterval(intervalId);
     });
   });
 
-  slider.addEventListener('mouseenter', function () {
+  slider.addEventListener("mouseenter", function () {
     clearInterval(intervalId);
   });
 
-  slider.addEventListener('mouseleave', function () {
+  slider.addEventListener("mouseleave", function () {
     intervalId = setInterval(autoSlide, 3500);
   });
 
   function changeSlide(index) {
-    const slideWidth = document.querySelector('.slide').offsetWidth;
+    const slideWidth = document.querySelector(".slide").offsetWidth;
     slider.style.transform = `translateX(-${index * slideWidth}px)`;
     currentIndex = index;
 
@@ -42,18 +42,18 @@ document.addEventListener('DOMContentLoaded', function () {
 //Slider
 
 //Body
-document.addEventListener('DOMContentLoaded', function () {
-  const blocks = document.querySelectorAll('.shop-latest__block');
+document.addEventListener("DOMContentLoaded", function () {
+  const blocks = document.querySelectorAll(".shop-latest__block");
 
   blocks.forEach(function (block) {
-    const imgElement = block.querySelector('.shop-latest__heart');
+    const imgElement = block.querySelector(".shop-latest__heart");
 
-    let currentImage = './assets/img/Home_img/Body/heart.png';
+    let currentImage = "./assets/img/Home_img/Body/heart.png";
 
-    imgElement.addEventListener('click', function () {
-      currentImage === './assets/img/Home_img/Body/heart.png'
-        ? (currentImage = './assets/img/Home_img/Body/red_heart.png')
-        : (currentImage = './assets/img/Home_img/Body/heart.png');
+    imgElement.addEventListener("click", function () {
+      currentImage === "./assets/img/Home_img/Body/heart.png"
+        ? (currentImage = "./assets/img/Home_img/Body/red_heart.png")
+        : (currentImage = "./assets/img/Home_img/Body/heart.png");
 
       imgElement.src = currentImage;
     });
@@ -61,30 +61,29 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // Shop page Team B
-const discountElements = document.querySelectorAll('.shop-latest__block');
+const discountElements = document.querySelectorAll(".shop-latest__block");
 
 discountElements.forEach((el) => {
-  let h1;
-  if (el.firstElementChild.classList.contains('add-discount')) {
-    let h = el.querySelector('.shop-latest__price');
-    h.firstElementChild.classList.add('price-discount');
-    h1 = h.textContent.trim();
+  const addDiscountElement = el.firstElementChild;
 
-    const element = el.querySelector('.add-discount');
-    const blockDiscount = window.getComputedStyle(element, '::before');
+  if (addDiscountElement.classList.contains("add-discount")) {
+    const priceElement = el.querySelector(".shop-latest__price");
+    priceElement.firstElementChild.classList.add("price-discount");
+
+    const h1 = priceElement.textContent.trim();
     const contentValue =
-      blockDiscount
-        .getPropertyValue('content')
-        .replace('%', '')
-        .replace('-', '')
-        .slice(1)
-        .replace('"', '') / 100;
+      parseFloat(
+        window
+          .getComputedStyle(addDiscountElement, "::before")
+          .getPropertyValue("content")
+          .replace(/["%-]/g, "")
+      ) / 100;
 
-    const h1value = parseFloat(h1.slice(2).replace(',', '.'));
-
+    const h1value = parseFloat(h1.slice(2).replace(",", "."));
     const disc = h1value - h1value * contentValue;
+
     const discount = `<a href="#">$ ${disc}</a>`;
-    h.insertAdjacentHTML('beforeend', discount);
+    priceElement.insertAdjacentHTML("beforeend", discount);
   }
 });
 
