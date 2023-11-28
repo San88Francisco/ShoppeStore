c396a9164e6f9bd2ba583dacd786d81ed0a36f56
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2023-11-28 23:19:48 +0200

    commit Ivan

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,59 +1,78 @@
-//Slider
-document.addEventListener("DOMContentLoaded", function () {
-  const slider = document.querySelector(".slider");
-  const radioButtons = document.querySelectorAll(
-    '.radio-buttons input[type="radio"]'
-  );
-  let currentIndex = 0;
-  let intervalId;
+// ||| Slider |||
 
-  radioButtons.forEach(function (radio, index) {
-    radio.addEventListener("change", function () {
-      changeSlide(index);
-      clearInterval(intervalId);
-    });
-  });
+// Створюємо наші зображення imgSlider
+const imgSlider = document.querySelectorAll('.slide');
+console.log('images:', imgSlider);
 
-  slider.addEventListener("mouseenter", function () {
-    clearInterval(intervalId);
-  });
+// Створюємо наші кружечки гортання
+const sliderDot = document.querySelectorAll('.radio-buttons label');
+console.log('sliderDot:', sliderDot);
 
-  slider.addEventListener("mouseleave", function () {
-    intervalId = setInterval(autoSlide, 3500);
-  });
+// Лічильник для слайдера
+let currentSlider = 0;
 
-  function changeSlide(index) {
-    const slideWidth = document.querySelector(".slide").offsetWidth;
-    slider.style.transform = `translateX(-${index * slideWidth}px)`;
-    currentIndex = index;
-
-    radioButtons.forEach(function (radio, radioIndex) {
-      radio.checked = radioIndex === currentIndex;
-    });
+// Функція яка включає наш слайдер
+function slider() {
+  for (let i = 0; i < imgSlider.length; i++) {
+    // тут ми проходимося по нашим зображенням та задаємо їм клас opacity0
+    imgSlider[i].classList.add('opacity0');
+    sliderDot[i].classList.remove('slider_dot');
   }
+  // тут ми видаляємо клас opacity0 з того зображення, що вказано в currentSlider, тобто таким чином завжди буде видно одне зображення на екрані
+  imgSlider[currentSlider].classList.remove('opacity0');
+  sliderDot[currentSlider].classList.add('slider_dot');
+  // перевірка якщо ми дійшли до останнього зображення, переходимо на перше зображення, або в else йдемо далі по зображеннях
+}
 
-  function autoSlide() {
-    currentIndex = (currentIndex + 1) % radioButtons.length;
-    changeSlide(currentIndex);
+slider();
+
+const rightSlider = () => {
+  // Якщо зображення останнє і я гортаю на вправо, тоді воно перегортає на перше
+  if (currentSlider + 1 === imgSlider.length) {
+    // якщо ця умова спрацювала, тоді ми присвоюємо currentSlider перше фото
+    currentSlider = 0;
+  } else {
+    // в іншому випадку просто додаємо і зображення прогортує вправо
+    currentSlider++;
   }
+  return slider();
+};
 
-  intervalId = setInterval(autoSlide, 3500);
+sliderDot.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    console.log('dot:', dot);
+    console.log('index:', index);
+    currentSlider = index;
+    slider();
+  });
 });
-//Slider
+
+function startTime() {
+  // Встановлюємо інтервал на 3000 мілісекунд (3 секунда)
+  setInterval(() => {
+    console.log('good');
+    rightSlider();
+  }, 5000);
+}
+
+// Викликаємо функцію startTime, щоб розпочати інтервал
+startTime();
+
+// ||| Slider |||
 
 //Body
-document.addEventListener("DOMContentLoaded", function () {
-  const blocks = document.querySelectorAll(".shop-latest__block");
+document.addEventListener('DOMContentLoaded', function () {
+  const blocks = document.querySelectorAll('.shop-latest__block');
 
   blocks.forEach(function (block) {
-    const imgElement = block.querySelector(".shop-latest__heart");
+    const imgElement = block.querySelector('.shop-latest__heart');
 
-    let currentImage = "./assets/img/Home_img/Body/heart.png";
+    let currentImage = './assets/img/Home_img/Body/heart.png';
 
-    imgElement.addEventListener("click", function () {
-      currentImage === "./assets/img/Home_img/Body/heart.png"
-        ? (currentImage = "./assets/img/Home_img/Body/red_heart.png")
-        : (currentImage = "./assets/img/Home_img/Body/heart.png");
+    imgElement.addEventListener('click', function () {
+      currentImage === './assets/img/Home_img/Body/heart.png'
+        ? (currentImage = './assets/img/Home_img/Body/red_heart.png')
+        : (currentImage = './assets/img/Home_img/Body/heart.png');
 
       imgElement.src = currentImage;
     });
