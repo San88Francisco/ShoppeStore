152b7bf3a8da8ebcf154848a96a0b4fc69ca04a9
Vasyl Verbytskyi <136508377+San88Francisco@users.noreply.github.com>
2023-11-28 22:31:25 +0100

    Revert "Andrij"

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,66 +1,67 @@
 //Slider
-document.addEventListener("DOMContentLoaded", function () {
-  const slider = document.querySelector(".slider");
-  const radioButtons = document.querySelectorAll(
-    '.radio-buttons input[type="radio"]'
-  );
-  let currentIndex = 0;
-  let intervalId;
+document.addEventListener('DOMContentLoaded', function () {
+   const slider = document.querySelector('.slider');
+   const radioButtons = document.querySelectorAll('.radio-buttons input[type="radio"]');
+   let currentIndex = 0;
+   let intervalId;
 
-  radioButtons.forEach(function (radio, index) {
-    radio.addEventListener("change", function () {
-      changeSlide(index);
-      clearInterval(intervalId);
-    });
-  });
+   radioButtons.forEach(function (radio, index) {
+       radio.addEventListener('change', function () {
+           changeSlide(index);
+           clearInterval(intervalId);
+       });
+   });
 
-  slider.addEventListener("mouseenter", function () {
-    clearInterval(intervalId);
-  });
+   slider.addEventListener('mouseenter', function () {
+       clearInterval(intervalId);
+   });
 
-  slider.addEventListener("mouseleave", function () {
-    intervalId = setInterval(autoSlide, 3500);
-  });
+   slider.addEventListener('mouseleave', function () {
+       intervalId = setInterval(autoSlide, 3500);
+   });
 
-  function changeSlide(index) {
-    const slideWidth = document.querySelector(".slide").offsetWidth;
-    slider.style.transform = `translateX(-${index * slideWidth}px)`;
-    currentIndex = index;
+   function changeSlide(index) {
+       const slideWidth = document.querySelector('.slide').offsetWidth;
+       slider.style.transform = `translateX(-${index * slideWidth}px)`;
+       currentIndex = index;
 
-    radioButtons.forEach(function (radio, radioIndex) {
-      radio.checked = radioIndex === currentIndex;
-    });
-  }
+       radioButtons.forEach(function (radio, radioIndex) {
+           radio.checked = radioIndex === currentIndex;
+       });
+   }
 
-  function autoSlide() {
-    currentIndex = (currentIndex + 1) % radioButtons.length;
-    changeSlide(currentIndex);
-  }
+   function autoSlide() {
+       currentIndex = (currentIndex + 1) % radioButtons.length;
+       changeSlide(currentIndex);
+   }
 
-  intervalId = setInterval(autoSlide, 3500);
+   intervalId = setInterval(autoSlide, 3500);
 });
 //Slider
 
+
 //Body
 document.addEventListener("DOMContentLoaded", function () {
-  const blocks = document.querySelectorAll(".shop-latest__block");
+    const blocks = document.querySelectorAll(".shop-latest__block");
 
-  blocks.forEach(function (block) {
-    const imgElement = block.querySelector(".shop-latest__heart");
+    blocks.forEach(function (block) {
 
-    let currentImage = "../../../assets/img/Home_img/Body/heart_bg.png";
+        const imgElement = block.querySelector(".shop-latest__heart");
 
-    imgElement.addEventListener("click", function () {
-      currentImage === "../../../assets/img/Home_img/Body/heart_bg.png"
-        ? (currentImage = "../../../assets/img/Home_img/Body/red_heart.png")
-        : (currentImage = "../../../assets/img/Home_img/Body/heart_bg.png");
+        let currentImage = './assets/img/Home_img/Body/heart.png';
 
-      imgElement.src = currentImage;
+        imgElement.addEventListener("click", function () {
+            if (currentImage === './assets/img/Home_img/Body/heart.png') {
+                currentImage = './assets/img/Home_img/Body/red_heart.png'; 
+            } else {
+                currentImage = './assets/img/Home_img/Body/heart.png';
+            }
+            
+            imgElement.src = currentImage;
+        });
     });
-  });
 });
 //Body
-
 
 
 // ---HEADER---
