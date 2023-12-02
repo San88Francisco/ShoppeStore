447b43b5a5024bfba7a59d9e1c2f052b1a26a32e
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2023-12-02 23:28:32 +0100

    I EBAV TAKY ROBOTY

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,19 +7,7 @@ import './Shop/Body'
 import './Shop/Aside'
 
 import './Product/AddToCart'
+import './Product/Reviews'
 
-// ---PRODUCT INHER PAGE(description,b)---
-document.addEventListener('DOMContentLoaded', () => {
-  const infoTabPage = document.querySelectorAll('.info__tab-page');
-  const tabButtons = document.querySelectorAll('.info__tab-page');
-  const productPages = document.querySelectorAll('.product__page');
-  tabButtons.forEach((item, index) => {
-      item.addEventListener('click', () => {
-          tabButtons.forEach(btn => btn.classList.remove('info__tab__line'));
-          item.classList.add('info__tab__line');
-          productPages.forEach(page => page.style.display = 'none');
-          productPages[index].style.display = 'grid';
-      });
-  });
-});
+
 
