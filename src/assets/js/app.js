e27690c2d7223803fc0cf5eaa2dae4cc4dce2dc2
Vasyl Verbytskyi <136508377+San88Francisco@users.noreply.github.com>
2023-12-03 12:56:18 +0100

    Revert "Shop conect pags"

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,30 +10,4 @@ import './Product/AddToCart'
 import './Product/Reviews'
 
 
-//! ====Search====
-document.querySelector('#search-input').oninput = function () {
-   let val = this.value.trim().toLowerCase();
-   let lettersItems = document.querySelectorAll('.shop-latest__name a');
-
-   lettersItems.forEach(function (e) {
-      const shopLatestBlock = e.closest('.shop-latest__block');
-
-      if (val == '') {
-         shopLatestBlock.style.display = 'block';
-
-      } else if (val !== '' && e.innerText.toLowerCase().includes(val)) {
-         shopLatestBlock.style.display = 'block';
-      } else {
-         shopLatestBlock.style.display = 'none';
-      }
-   });
-}
-//! ====Search====
-
-
-
-
-
-
-
 
