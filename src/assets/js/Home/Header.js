6fa3c9a5eafe2de32978c442491c0350c31a46df
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2023-12-04 16:22:05 +0100

    deBag

--- src/assets/js/Home/Header.js
+++ src/assets/js/Home/Header.js
@@ -1,10 +1,10 @@
 // ---HEADER---
 function setupHeader() {
-  const header__underline = document.querySelector(".header__underline");
-  const pageName = document.title.toLowerCase().replace(/\s/g, "");
-  const headerAElement = document.querySelector(`.header_link_${pageName}`);
-  headerAElement.classList.add("act");
-  pageName !== "home" ? (header__underline.style.display = "block") : 0;
+   const header__underline = document.querySelector(".header__underline");
+   const pageName = document.title.toLowerCase().replace(/\s/g, "");
+   pageName !== "home" ? (header__underline.style.display = "block") : 0;
+   const headerAElement = document.querySelector(`.header_link_${pageName}`);
+   headerAElement.classList.add("act");
 }
 
 document.addEventListener("DOMContentLoaded", setupHeader);
