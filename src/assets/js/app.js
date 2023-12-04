e343e02a22f336354c30e224dc15b56d9a05082d
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2023-12-04 09:37:23 +0100

    upDate

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,14 +1,14 @@
-import './Product/Reviews'
-import './Product/AddToCart'
+// import './Home/Header'
+// import './Home/Slider'
+// import './Home/Body'
 
-import './Home/Header'
-import './Home/Slider'
-import './Home/Body'
+// import './Shop/Body'
+// import './Shop/Aside'
 
-import './Shop/Body'
-import './Shop/Aside'
+// import './Product/AddToCart'
+// import './Product/Reviews'
 
-
+import './Contact/ContactUs'
 
 
 
