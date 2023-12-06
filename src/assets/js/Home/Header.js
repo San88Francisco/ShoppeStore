a99ef9cbee78c98961795389ab9e40ef45d31e28
Xlussov <130926999+Xlussov@users.noreply.github.com>
2023-12-06 21:43:54 +0200

    i feneshed created tab-page block

--- src/assets/js/Home/Header.js
+++ src/assets/js/Home/Header.js
@@ -3,8 +3,10 @@ function setupHeader() {
   const header__underline = document.querySelector(".header__underline");
   const pageName = document.title.toLowerCase().replace(/\s/g, "");
   const headerAElement = document.querySelector(`.header_link_${pageName}`);
-  headerAElement.classList.add("act");
+  headerAElement !== null ? headerAElement.classList.add("act") : 0
   pageName !== "home" ? (header__underline.style.display = "block") : 0;
+  console.log(pageName);
 }
+
 
 document.addEventListener("DOMContentLoaded", setupHeader);
