4df761e017be46beb9b57a2093c5257edac8f259
Andrii Dmytruk <admitruk237@gmail.com>
2023-12-11 11:53:48 +0100

    changeAcount

--- src/assets/js/Account/SingIn.js
+++ src/assets/js/Account/SingIn.js
@@ -1,18 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Переконайтеся, що елементи існують перед викликом addEventListener
-  const clearIconMail = document.querySelector(".clear-icon_mail");
-  const clearIconPassword = document.querySelector(".clear-icon_pass");
-  const inputMail = document.querySelector(".input_mail");
-  const inputPassword = document.querySelector(".input_pass");
 
-  if (clearIconMail && clearIconPassword && inputMail && inputPassword) {
-    // Додайте слухачів подій тільки якщо елементи знайдені
-    clearIconMail.addEventListener("click", function () {
-      inputMail.value = "";
-    });
+// Отримуємо посилання на потрібні елементи DOM
+const signInTab = document.getElementById('signInTab');
+const registerTab = document.getElementById('registerTab');
+const signInBlock = document.querySelector('.block-tabs--singIn');
+const registerBlock = document.querySelector('.block-tabs--register');
 
-    clearIconPassword.addEventListener("click", function () {
-      inputPassword.value = "";
-    });
-  }
+// Функція для зміни активної вкладки та відображення блоків
+function switchTabs(activeTab, inactiveTab, activeBlock, inactiveBlock) {
+   activeTab.classList.add('_active');
+   inactiveTab.classList.remove('_active');
+   activeBlock.style.display = 'block';
+   inactiveBlock.style.display = 'none';
+
+   setTimeout(() => {
+      activeBlock.classList.add('opac');
+   }, 0.); // Застосувати зміни через 0.1 секунди (після з'явлення блоку)
+
+   inactiveBlock.classList.remove('opac'); // Видалення класу 'opac' з неактивного блоку одразу
+}
+
+// Додаємо клас '_active' до елемента по замовчуванню
+signInTab.classList.add('_active');
+
+// При кліку на вкладку "Sign in"
+signInTab.addEventListener('click', function (event) {
+   event.preventDefault(); // Запобігаємо стандартному переходу за посиланням
+   switchTabs(signInTab, registerTab, signInBlock, registerBlock);
+});
+
+// При кліку на вкладку "Register"
+registerTab.addEventListener('click', function (event) {
+   event.preventDefault(); // Запобігаємо стандартному переходу за посиланням
+   switchTabs(registerTab, signInTab, registerBlock, signInBlock);
 });
