76451781ca0726652da8add2e7abc00e38d567dd
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2023-12-14 19:23:24 +0200

    100%

--- src/assets/js/Product/PopupWindow.js
+++ src/assets/js/Product/PopupWindow.js
@@ -8,48 +8,64 @@ document.addEventListener('DOMContentLoaded', function () {
     const amountAddCart = document.querySelector('.amount');
     const navCartCount = document.querySelector('.cart__count');
 
+    // Створюємо змінну яка приймає в себе кількість товарів в кошику, якщо в localStorage пусто, буде 0
     let totalCountCart = localStorage.getItem('totalCountCart') || 0;
+
+    // Створюємо змінну яка приймає в себе кількість продуктів в кошику, якщо в localStorage пусто, буде пустий масив
     let allProductCart =
       JSON.parse(localStorage.getItem('allProductCart')) || [];
 
     function handleClick() {
-      btnAddtoCart.removeEventListener('click', handleClick); // Видаляємо обробник подій
+      // Видаляємо обробник подій, кнопка стає не активна, щоб поки не закінчиться функція ми не могли знову додавати товари в кошик
+      btnAddtoCart.removeEventListener('click', handleClick);
       btnAddtoCart.disabled = true;
+      // Задаємо стилі для popupWindow і headerUnderline
       headerUnderline.style.border = '1px solid #A18A68';
       headerUnderline.style.transition = 'border 0.3s ease-in-out';
       popupWindow.style.visibility = 'visible';
       popupWindow.style.opacity = 1;
       popupWindow.style.transition = 'opacity 0.3s ease-in-out';
 
+      // Перевірка для правильного відображення загальної кількості товарів в кошику
       if (totalCountCart === 0) {
+        // Якщо в кошику пусто, це означає що перше значення буде яке ми виберемо в amount
         totalCountCart = +amountAddCart.textContent;
         navCartCount.style.display = 'flex';
         navCartCount.textContent = +amountAddCart.textContent;
+        // Записуємо перше значення totalCountCart в localStorage
         localStorage.setItem('totalCountCart', totalCountCart);
       } else {
+        // Якщо в localStorage вже є якась кількість, тоді ми просто додаємо amount і totalCountCart
         totalCountCart =
           parseInt(totalCountCart) + parseInt(amountAddCart.textContent);
         navCartCount.textContent = totalCountCart;
         localStorage.setItem('totalCountCart', totalCountCart);
       }
+      // Якщо в кошику більше 99 товарів, ми завжди будемо відображати 99
       if (totalCountCart > 100) {
         navCartCount.textContent = 99;
       }
-      productInfo(+amountAddCart.textContent);
-      console.log(+amountAddCart.textContent);
-      amountAddCart.textContent = 1;
-      console.log(+amountAddCart.textContent);
 
+      // Функція яка створює обєкт з товаром. Тут ми передаємо кількість вибраного товару amountAddCart
+      productInfo(+amountAddCart.textContent);
+      // Перезаписуємо amount на 1
+      amountAddCart.textContent = 1;
+
+      // Функція яка через 5 секунд приховає popupWindow та підправить стилі з headerUnderline
       setTimeout(() => {
         headerUnderline.style.border = '1px solid #d8d8d8';
         popupWindow.style.opacity = '0';
         btnAddtoCart.disabled = false; // Знову включаємо кнопку
-        btnAddtoCart.addEventListener('click', handleClick); // Додаємо обробник подій
+
+        // Додаємо обробник подій, аби кнопка стала активна і можна було додати знову товар
+        btnAddtoCart.addEventListener('click', handleClick);
       }, 3000);
     }
 
+    // Подія що запускає додавання товару в кошик
     btnAddtoCart.addEventListener('click', handleClick);
 
+    // Тут ми створюємо наший обєкт та відправляємо його в localStorage
     const productInfo = (amount) => {
       const productName = document.querySelector(
         '.product--overview__view h3'
@@ -67,8 +83,8 @@ document.addEventListener('DOMContentLoaded', function () {
         (product) => product.name === productName
       );
 
+      // Якщо знайдено, змінюємо amount для існуючого продукту
       if (existingProductIndex !== -1) {
-        // Якщо знайдено, змінюємо amount для існуючого продукту
         allProductCart[existingProductIndex].count += amount;
       } else {
         // Якщо не знайдено, додаємо новий об'єкт до масиву
