7888bb16faaa72502e6fef174083df7806e21204
Andrij <Andrijpotapov1@gmail.com>
2023-12-16 15:38:44 +0200

    анімації до бургера

--- src/assets/js/Home/Burger.js
+++ src/assets/js/Home/Burger.js
@@ -16,3 +16,21 @@ function setupHeader() {
   
   document.addEventListener("DOMContentLoaded", setupHeader);
   
+  document.addEventListener('DOMContentLoaded', function () {
+    const checkbox = document.querySelector('.checkbox');
+    const menuItems = document.querySelector('.menu-items');
+    const burgerItems = document.querySelector('.burger-list');
+    const burgerHeader = document.querySelector('.burger__header');
+    const burgerFooter = document.querySelector('.burger__footer');
+    const burgerLogout = document.querySelector('.burger__logout');
+   
+    checkbox.addEventListener('click', function () {
+        this.classList.toggle('active');
+        menuItems.classList.toggle('active');
+        burgerItems.classList.toggle('active');
+        burgerFooter.classList.toggle('active');
+        burgerLogout.classList.toggle('active');
+        burgerHeader.classList.toggle('active');
+    });
+});
+
