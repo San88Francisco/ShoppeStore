edbb6e97270466eecd80316a5ea721375e172784
Andrij <Andrijpotapov1@gmail.com>
2023-12-18 15:23:44 +0200

    фікс футера, хедера, дороблення бургера

--- src/assets/js/Home/Burger.js
+++ src/assets/js/Home/Burger.js
@@ -19,18 +19,33 @@ function setupHeader() {
   document.addEventListener('DOMContentLoaded', function () {
     const checkbox = document.querySelector('.checkbox');
     const menuItems = document.querySelector('.menu-items');
-    const burgerItems = document.querySelector('.burger-list');
+    const burgerItem1 = document.querySelector('.burger-menu__item:nth-child(1)');
+    const burgerItem2 = document.querySelector('.burger-menu__item:nth-child(2)');
+    const burgerItem3 = document.querySelector('.burger-menu__item:nth-child(3)');
+    const burgerItem4 = document.querySelector('.burger-menu__item:nth-child(4)');
+    const burgerItem5 = document.querySelector('.burger-menu__item:nth-child(5)');
+    const burgerItem6 = document.querySelector('.burger-menu__item:nth-child(6)');
+    const burgerItem7 = document.querySelector('.burger-menu__item:nth-child(7)');
+   
     const burgerHeader = document.querySelector('.burger__header');
     const burgerFooter = document.querySelector('.burger__footer');
     const burgerLogout = document.querySelector('.burger__logout');
-   
+    const burgerLine = document.querySelector('.burger-line');
+
     checkbox.addEventListener('click', function () {
         this.classList.toggle('active');
         menuItems.classList.toggle('active');
-        burgerItems.classList.toggle('active');
+        burgerItem1.classList.toggle('active');
+        burgerItem2.classList.toggle('active');
+        burgerItem3.classList.toggle('active');
+        burgerItem4.classList.toggle('active');
+        burgerItem5.classList.toggle('active');
+        burgerItem6.classList.toggle('active');
+        burgerItem7.classList.toggle('active');
         burgerFooter.classList.toggle('active');
         burgerLogout.classList.toggle('active');
         burgerHeader.classList.toggle('active');
+        burgerLine.classList.toggle('active');
     });
 });
 
