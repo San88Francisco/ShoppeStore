99a17674f1dba4d2e430db16d2c225e45967a10f
Xlussov <130926999+Xlussov@users.noreply.github.com>
2023-12-20 01:56:32 +0200

    i created base blog page

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,16 +1,18 @@
-import "./Home/Header";
-import "./Home/Slider";
-import "./Home/Body";
-import "./Home/Burger";
+// import "./Home/Header";
+// import "./Home/Slider";
+// import "./Home/Body";
+// import "./Home/Burger";
 
-import "./Shop/Body";
-import "./Shop/Filter";
-import "./Shop/SortBy";
+// import "./Shop/Body";
+// import "./Shop/Filter";
+// import "./Shop/SortBy";
 
-import "./Product/AddToCart";
-import "./Product/Reviews";
+// import "./Product/AddToCart";
+// import "./Product/Reviews";
 
-import "./Contact/ContactUs";
+// import "./Contact/ContactUs";
 
-import "./Account/SingIn"; 
-import "./Account/ResetPassword"; 
+// import "./Account/SingIn"; 
+// import "./Account/ResetPassword"; 
+
+import "./Blog/Main-blog"; 
