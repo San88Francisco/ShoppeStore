f9c6be6aafe17f04d19538ddb5893d0a9a4141a5
Vasyl Verbytskyi <136508377+San88Francisco@users.noreply.github.com>
2023-12-20 14:40:16 +0100

    Revert "Cart pages"

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,21 +1,18 @@
-import './Home/Header';
-import './Home/Slider';
-import './Home/Body';
+import "./Home/Header";
+import "./Home/Slider";
+import "./Home/Body";
+import "./Home/Burger";
 
+import "./Shop/Body";
+import "./Shop/Filter";
+import "./Shop/SortBy";
 
-import './Shop/Body';
-import './Shop/Filter';
-import './Shop/SortBy';
+import "./Product/AddToCart";
+import "./Product/Reviews";
 
-import './Product/AddToCart';
-import './Product/Reviews';
-import './Product/PopupWindow';
+import "./Contact/ContactUs";
 
-import './Contact/ContactUs';
+import "./Account/SingIn"; 
+import "./Account/ResetPassword";
 
-import './Account/SingIn';
-import './Account/ResetPassword';
-
-import './Cart/Option';
-import './Cart/CartItem';
-
+import "./MyAccount/MyAccountMain"; 
