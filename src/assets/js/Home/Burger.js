e3ba4a40170199c1eccbcc50b83266bdfe295957
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2023-12-21 16:04:39 +0100

    create pags Help

--- src/assets/js/Home/Burger.js
+++ src/assets/js/Home/Burger.js
@@ -1,37 +1,37 @@
 function setupHeader() {
-  const pageName = document.title.toLowerCase().replace(/\s/g, "");
-  if (pageName === "home") return;
+   const pageName = document.title.toLowerCase().replace(/\s/g, "");
+   if (pageName === "home") return;
 
-  const burger__underline = document.querySelector(".burger__underline");
-  burger__underline.style.display = "none";
+   const burger__underline = document.querySelector(".burger__underline");
+   burger__underline.style.display = "none";
 
-  const headerAElement = document.querySelector(`.burger__link_${pageName}`);
-  headerAElement?.classList.add("burg");
-  pageName !== "home" && (burger__underline.style.display = "none");
+   const headerAElement = document.querySelector(`.burger__link_${pageName}`);
+   headerAElement?.classList.add("burg");
+   pageName !== "home" && (burger__underline.style.display = "none");
 }
 
 document.addEventListener("DOMContentLoaded", setupHeader);
 
 document.addEventListener("DOMContentLoaded", function () {
-  const checkbox = document.querySelector(".checkbox");
-  const menuItems = document.querySelector(".menu-items");
-  const burgerItems = document.querySelectorAll(".burger-menu__item");
-  const burgerHeader = document.querySelector(".burger__header");
-  const burgerFooter = document.querySelector(".burger__footer");
-  const burgerLogout = document.querySelector(".burger__logout");
-  const burgerLine = document.querySelector(".burger-line");
+   const checkbox = document.querySelector(".checkbox");
+   const menuItems = document.querySelector(".menu-items");
+   const burgerItems = document.querySelectorAll(".burger-menu__item");
+   const burgerHeader = document.querySelector(".burger__header");
+   const burgerFooter = document.querySelector(".burger__footer");
+   const burgerLogout = document.querySelector(".burger__logout");
+   const burgerLine = document.querySelector(".burger-line");
 
-  checkbox.addEventListener("click", function () {
-    this.classList.toggle("active");
-    menuItems.classList.toggle("active");
+   checkbox.addEventListener("click", function () {
+      this.classList.toggle("active");
+      menuItems.classList.toggle("active");
 
-    burgerItems.forEach((item) => {
-      item.classList.toggle("active");
-    });
+      burgerItems.forEach((item) => {
+         item.classList.toggle("active");
+      });
 
-    burgerFooter.classList.toggle("active");
-    burgerLogout.classList.toggle("active");
-    burgerHeader.classList.toggle("active");
-    burgerLine.classList.toggle("active");
-  });
+      burgerFooter.classList.toggle("active");
+      burgerLogout.classList.toggle("active");
+      burgerHeader.classList.toggle("active");
+      burgerLine.classList.toggle("active");
+   });
 });
