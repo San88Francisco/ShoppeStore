b92b13b762ac8a72278f4c0ff1fd3712022519dc
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2023-12-28 18:23:10 +0100

    404

--- src/assets/js/Error/Error404.js
+++ src/assets/js/Error/Error404.js
@@ -1,1 +1,19 @@
-console.log('good');
+const PAGE_NOT_FOUND = "error404.html";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname.split("/").pop().replace(".html", "");
+    const pageExists = document.querySelector(`#${currentPage}`);
+
+    // Перевірка, чи сторінка 404 вже відображається
+    const is404Page = currentPage.toLowerCase() === "error404";
+
+    if (!pageExists && !is404Page) {
+        redirectTo404();
+    }
+
+    function redirectTo404() {
+        window.location.href = PAGE_NOT_FOUND;
+    }
+});
+
+
