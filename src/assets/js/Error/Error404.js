a9faa8aeb4823401bafca717b91eabd8c169e991
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2023-12-28 19:22:22 +0100

    404

--- src/assets/js/Error/Error404.js
+++ src/assets/js/Error/Error404.js
@@ -1,11 +1,14 @@
+const PAGES = ["index", "shop", "contact","index", "shop", "contact"]; // Додайте імена всіх існуючих сторінок
 const PAGE_NOT_FOUND = "error404.html";
 
 document.addEventListener("DOMContentLoaded", function () {
     const currentPage = window.location.pathname.split("/").pop().replace(".html", "");
-    const pageExists = document.querySelector(`#${currentPage}`);
 
     // Перевірка, чи сторінка 404 вже відображається
     const is404Page = currentPage.toLowerCase() === "error404";
+
+    // Перевірка, чи поточна сторінка належить до списку існуючих сторінок
+    const pageExists = PAGES.includes(currentPage);
 
     if (!pageExists && !is404Page) {
         redirectTo404();
