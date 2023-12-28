8e30708c7ec6a08102b62516ece85e73c467dbb1
Xlussov <130926999+Xlussov@users.noreply.github.com>
2023-12-28 14:13:49 +0200

    debag underline on header

--- src/assets/js/Home/Header.js
+++ src/assets/js/Home/Header.js
@@ -17,6 +17,8 @@ function setupHeader() {
   const headerAElement = document.querySelector(`.header_link_${pageName}`);
   headerAElement !== null ? headerAElement.classList.add("act") : 0
   pageName === 'account' &&  headerAElement !== null ? headerAElement.classList.add("act__last") : 0
+  console.log(headerAElement);
+  pageName === 'myaccount' &&  headerAElement !== null ? headerAElement.classList.add("act__last") : 0
 
   pageName !== "home" ? (header__underline.style.display = "block") : 0;
 }
