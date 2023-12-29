4f3693f462884cd0ba95e9b579188488439a1a11
Володимир Кирилюк <donatikyt23@gmail.com>
2023-12-29 16:16:20 +0200

    test

--- src/assets/js/Error/Error404.js
+++ src/assets/js/Error/Error404.js
@@ -1,11 +1,15 @@
+const PAGES = ["index", "shop", "blog","cart", "account", "contact","checkout-pages",
+               "help","my-account","our-story","product","reset-password"]; // Додайте імена всіх існуючих сторінок
 const PAGE_NOT_FOUND = "error404.html";
 
 document.addEventListener("DOMContentLoaded", function () {
     const currentPage = window.location.pathname.split("/").pop().replace(".html", "");
-    const pageExists = document.querySelector(`#${currentPage}`);
 
     // Перевірка, чи сторінка 404 вже відображається
     const is404Page = currentPage.toLowerCase() === "error404";
+
+    // Перевірка, чи поточна сторінка належить до списку існуючих сторінок
+    const pageExists = PAGES.includes(currentPage);
 
     if (!pageExists && !is404Page) {
         redirectTo404();
@@ -15,5 +19,3 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href = PAGE_NOT_FOUND;
     }
 });
-
-
