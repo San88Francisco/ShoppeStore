0c1856456ddfd830026e4fb325ddcc6fce9c9921
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2023-12-29 11:23:19 +0100

    test

--- src/assets/js/Error/Error404.js
+++ src/assets/js/Error/Error404.js
@@ -1,22 +1,26 @@
-const PAGES = ["index", "shop", "contact", "index", "shop", "contact"]; // Додайте імена всіх існуючих сторінок
-const PAGE_NOT_FOUND = "error404.html";
 
-document.addEventListener("DOMContentLoaded", function () {
-   const currentPage = window.location.pathname.split("/").pop().replace(".html", "");
+document.addEventListener('DOMContentLoaded', function () {
 
-   // Перевірка, чи сторінка 404 вже відображається
-   const is404Page = currentPage.toLowerCase() === "error404";
-
-   // Перевірка, чи поточна сторінка належить до списку існуючих сторінок
-   const pageExists = PAGES.includes(currentPage);
-
-   if (!pageExists && !is404Page) {
-      redirectTo404();
-   }
-
-   function redirectTo404() {
-      window.location.href = PAGE_NOT_FOUND;
-   }
-});
+   const item = JSON.parse(localStorage.getItem('allProduct'));
+   console.log('✌️item --->', item);
 
 
+   document.querySelector('#burber-search').oninput = function () {
+      if (val == '') {
+         shopLatestBlock2.style.display = 'block';
+      } else if (val !== '' && e.innerText.toLowerCase().includes(val)) {
+         shopLatestBlock2.style.display = 'block';
+      } else {
+         shopLatestBlock2.style.display = 'none';
+      }
+      let val = this.value.trim().toLowerCase();
+      console.log('✌️val --->', val);
+      // let lettersItems = document.querySelectorAll('.shop-latest__name a');
+      let burgerList = document.querySelector('.burger-list');
+
+      lettersItems.forEach(function (e) {
+         const shopLatestBlock2 = e.closest('.shop-latest__block');
+
+      });
+   };
+})
