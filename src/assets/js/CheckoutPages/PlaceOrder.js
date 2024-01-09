831b13aef5cca67306018382cc5bc22935b2449d
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2024-01-09 21:46:39 +0200

    validation processing code

--- src/assets/js/CheckoutPages/PlaceOrder.js
+++ src/assets/js/CheckoutPages/PlaceOrder.js
@@ -27,61 +27,78 @@ export const placeOrder = (paymentMethod) => {
 
   // console.log('blockData', blockData);
 
-  let isValid = false;
+  const inputHandler = ({ target }) => {
+    if (target.hasAttribute('data-reg')) {
+      inputCheck(target);
+      console.log('target:', target);
+    }
+  };
 
-  const errorName = document.querySelector('.order__error-name');
-  console.log('errorName:', errorName);
+  const form = document.forms['checkout__container'];
+  console.log('form:', form);
 
-  // const inputFields = document.querySelectorAll('.checkout__block-name input');
-  // inputFields.forEach((inputField, index) => {
-  //   inputField.addEventListener('input', function () {
-  //     console.log(inputField);
-  //     if (inputFirstName.trim().length < 4) {
-  //       console.log('Спрацювала inputFirstName');
-  //       errorName.style.setProperty('opacity', '1', 'important');
-  //     } else {
-  //       console.log('Введено імя');
-  //       errorName.style.opacity = '0';
-  //     }
-  //   });
-  // });
+  form.addEventListener('input', inputHandler);
 
-  // Name validation
+  const inputCheck = (element) => {
+    console.log('element', element);
+    const inputValue = element.value;
+    const inputReg = element.getAttribute('data-reg');
+    const reg = new RegExp(inputReg);
 
-  // if (inputLastName.trim().length < 4) {
-  //   console.log('Спрацювала inputLastName');
-  //   return (isValid = false);
-  // }
-  // if (inputCountry.trim().length < 6) {
-  //   console.log('Спрацювала inputCountry');
-  //   return (isValid = false);
-  // }
-  // if (inputCity.trim().length < 4) {
-  //   console.log('Спрацювала inputCity');
-  //   return (isValid = false);
-  // }
-  // console.log(inputPostcode);
-  // if (inputPostcode) {
-  //   console.log('Спрацювала inputPostcode');
-  //   isValid = false;
-  //   return; // Додаємо return, щоб вийти з функції
-  // }
-  // if (inputPhone.trim().length < 2) {
-  //   console.log('Спрацювала inputPhone');
-  //   return (isValid = false);
-  // }
-  // if (inputEmail.trim().length < 2) {
-  //   console.log('Спрацювала inputEmail');
-  //   return (isValid = false);
-  // }
-
-  if (isValid) {
-    console.log('Form submitted successfully!');
-    // Add code to submit the form or perform other actions here
-  }
+    if (reg.test(inputValue)) {
+      element.style.border = '2px solid rgb(0,196,0)';
+    } else {
+      element.style.border = '2px solid rgb(255,0,0)';
+    }
+  };
 
   localStorage.setItem('userOrderInfo', JSON.stringify(blockData));
   // saveOrder();
   // clearOrderCart();
   // window.location.href = 'http://localhost:3000/order-confirmation.html';
 };
+
+// let isValid = false;
+
+//   const errorName = document.querySelector('.order__error-name');
+
+//   const inputFields = document.querySelectorAll('.checkout__block-name input');
+
+//   const inputValidate = () => {};
+
+//   inputFields.forEach((inputField, index) => {
+//     if (inputFirstName.length < 2) {
+//       console.log(inputField);
+//       console.log(index);
+//       return console.log('немає inputFirstName');
+//     }
+//     if (inputLastName.length < 2) {
+//       return console.log('немає inputLastName');
+//     }
+//     if (inputCountry.length < 2) {
+//       return console.log('немає inputCountry');
+//     }
+//     if (inputCity.length < 2) {
+//       return console.log('немає inputCity');
+//     }
+//     if (inputStreet.length < 2) {
+//       return console.log('немає inputStreet');
+//     }
+//     if (inputPostcode.length < 2) {
+//       return console.log('немає inputPostcode');
+//     }
+//     if (inputPhone.length < 2) {
+//       return console.log('немає inputPhone');
+//     }
+//     if (inputEmail.length < 2) {
+//       return console.log('немає inputEmail');
+//     }
+//     // inputField.addEventListener('input', function () {
+//     //   console.log(inputField);
+//     // });
+//   });
+
+//   if (isValid) {
+//     console.log('Form submitted successfully!');
+//     // Add code to submit the form or perform other actions here
+//   }
