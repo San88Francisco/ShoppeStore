a7390c1b0bde8f136f734a8a4c59e72c184cdbc0
Akumuuu <88854566+Akumuuu@users.noreply.github.com>
2024-01-12 16:37:02 +0100

    account menu + logistics

--- src/assets/js/Error/Error404.js
+++ src/assets/js/Error/Error404.js
@@ -1,36 +1,50 @@
+import { updateAccountLinksVisibility } from "../Account/SingIn.js";
+import { handleUserSignIn } from "../Account/SingIn.js";
+
 const PAGES = [
-  'index',
-  'shop',
-  'blog',
-  'cart',
-  'account',
-  'contact',
-  'checkout-pages',
-  'help',
-  'my-account',
-  'our-story',
-  'product',
-  'reset-password',
-  'blog-addopt',
-  'order-confirmation',
-  '',
+  "index",
+  "shop",
+  "blog",
+  "cart",
+  "account",
+  "contact",
+  "checkout-pages",
+  "help",
+  "my-account",
+  "our-story",
+  "product",
+  "reset-password",
+  "blog-addopt",
+  "order-confirmation",
+  "",
 ]; // Додайте імена всіх існуючих сторінок
-const PAGE_NOT_FOUND = 'error404.html';
+const PAGE_NOT_FOUND = "error404.html";
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   const currentPage = window.location.pathname
-    .split('/')
+    .split("/")
     .pop()
-    .replace('.html', '');
+    .replace(".html", "");
 
-  // Перевірка, чи сторінка 404 вже відображається
-  const is404Page = currentPage.toLowerCase() === 'error404';
-
-  // Перевірка, чи поточна сторінка належить до списку існуючих сторінок
+  const is404Page = currentPage.toLowerCase() === "error404";
   const pageExists = PAGES.includes(currentPage);
 
   if (!pageExists && !is404Page) {
     redirectTo404();
+  } else {
+    updateAccountLinksVisibility();
+
+    const { redirectToLoginPage, handleLogout, userSignedIn } =
+      handleUserSignIn();
+
+    if (currentPage === "my-account" && !userSignedIn) {
+      redirectToLoginPage();
+    }
+
+    const logoutLink = document.getElementById("logout");
+    if (logoutLink) {
+      logoutLink.addEventListener("click", handleLogout);
+    }
   }
 
   function redirectTo404() {
