7c88ddb968893e93ccef8969f957b4b80871e421
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2024-01-28 17:55:53 +0200

    95%

--- src/assets/js/Home/LinkAddToCart.js
+++ src/assets/js/Home/LinkAddToCart.js
@@ -1,26 +1,79 @@
 export const LinkAddToCart = (contents) => {
-  console.log('contents:', contents);
+  // console.log('contents:', contents);
   let allProductCart = JSON.parse(localStorage.getItem('allProductCart')) || [];
+  let totalCountCart = localStorage.getItem('totalCountCart') || 0;
 
-  const addProductToCart = (itemBlock, productName) => {
-    console.log('product:', productName);
+  const navCartCount = document.querySelector('.cart__count');
+
+  const popupWindow = document.querySelector('.item__window-block');
+  const headerUnderline = document.querySelector('header');
+
+  const addProductToCart = (itemPrice, productName) => {
+    headerUnderline.style.borderBottom = '1.5px solid #A18A68';
+    headerUnderline.style.transition = 'border 0.3s ease-in-out';
+    popupWindow.style.visibility = 'visible';
+    popupWindow.style.opacity = 1;
+    popupWindow.style.transition = 'opacity 0.3s ease-in-out';
+
+    setTimeout(() => {
+      headerUnderline.style.borderBottom = '1.5px solid #d8d8d8';
+      popupWindow.style.opacity = '0';
+    }, 3000);
+
     const foundProduct = contents.find((item) => {
       return item.name === productName;
     });
-    console.log(foundProduct);
+    console.log('foundProduct:', foundProduct.amount);
 
-    console.log(itemBlock);
+    if (totalCountCart === '0' || !totalCountCart) {
+      // Якщо в кошику пусто, це означає що перше значення буде яке ми виберемо в amount
+      totalCountCart = foundProduct.amount;
+      navCartCount.style.display = 'flex';
+      navCartCount.textContent = foundProduct.amount;
+      // Записуємо перше значення totalCountCart в localStorage
+      localStorage.setItem('totalCountCart', totalCountCart);
+    } else {
+      // Якщо в localStorage вже є якась кількість, тоді ми просто додаємо amount і totalCountCart
+      totalCountCart = parseInt(totalCountCart) + foundProduct.amount;
+      navCartCount.textContent = totalCountCart;
+      localStorage.setItem('totalCountCart', totalCountCart);
+    }
 
-    const newProduct = {
-      name: foundProduct.name,
-      /** Перевірка чи товар має знижку, якщо ні, то присвоюємо просто ціну */
-      //   price: foundProduct.discountPrice ? foundProduct.textContent : productPrice,
-      /** Перевірка чи це знижка, якщо ні то записуємо пусту строку */
-      //   discountPrice: discount ? discount.textContent : '',
-      imgProduct: foundProduct.imageUrl,
-      count: foundProduct.amount,
-    };
-    allProductCart.push(newProduct);
+    // console.log(foundProduct);
+
+    // Отримати текст з першого елементу <a>
+    const firstPrice =
+      itemPrice.querySelector('.price-discount') ||
+      itemPrice.querySelector('a');
+
+    // // Отримати текст з другого елементу <a>
+    const secondPrice = itemPrice.querySelector('.discount');
+
+    // console.log('First Price:', firstPrice);
+    // console.log('Second Price:', secondPrice);
+
+    const existingProductIndex = allProductCart.findIndex(
+      (product) => product.name === productName
+    );
+    // console.log('existingProductIndex:', existingProductIndex);
+
+    if (existingProductIndex !== -1) {
+      allProductCart[existingProductIndex].count += foundProduct.amount;
+      console.log(allProductCart);
+    } else {
+      const newProduct = {
+        name: foundProduct.name,
+        /** Перевірка чи товар має знижку, якщо ні, то присвоюємо просто ціну */
+        price: firstPrice.textContent,
+        /** Перевірка чи це знижка, якщо ні то записуємо пусту строку */
+        discountPrice: secondPrice ? secondPrice.textContent : '',
+        imgProduct: foundProduct.imageUrl,
+        count: foundProduct.amount,
+      };
+      // console.log(newProduct);
+      allProductCart.push(newProduct);
+      console.log(allProductCart);
+    }
 
     localStorage.setItem('allProductCart', JSON.stringify(allProductCart));
   };
@@ -28,24 +81,15 @@ export const LinkAddToCart = (contents) => {
   const shopLatestHoverContainer = document.querySelectorAll(
     '.shop-latest_hover_container'
   );
-  //   const discountPrice = document.querySelectorAll(
-  //     '.shop-latest__price .price-discount'
-  //   );
-  //   console.log('  discountPrice:', discountPrice);
-  //   // Створюємо змінну із знижкою
-  //   const discount = document.querySelectorAll('.shop-latest__price .discount');
-  //   console.log('  discount:', discount);
-  //   const productPrice = document.querySelectorAll('.shop-latest__price a');
-  //   console.log(productPrice);
 
-  const shopLatestBlock = document.querySelectorAll('.shop-latest__block');
+  const shopLatestPrice = document.querySelectorAll('.shop-latest__price');
 
   const shopLatestName = document.querySelectorAll('.shop-latest__name');
 
   shopLatestHoverContainer.forEach((link, index) => {
     link.addEventListener('click', () => {
       addProductToCart(
-        shopLatestBlock[index],
+        shopLatestPrice[index],
         shopLatestName[index].textContent
       );
     });
