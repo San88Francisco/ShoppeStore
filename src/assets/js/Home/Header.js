78105e490f0dbb405ba19e566db021101cbcd9dc
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2024-01-29 14:55:31 +0100

    deBug

--- src/assets/js/Home/Header.js
+++ src/assets/js/Home/Header.js
@@ -1,87 +1,93 @@
 document.addEventListener('DOMContentLoaded', function () {
-  /** Count Icon Header */
+   /** Count Icon Header */
 
-  const navCartCount = document.querySelector('.cart__count');
+   const navCartCount = document.querySelector('.cart__count');
 
-  // Оновлення navCartCount на основі значення в localStorage
-  function updateNavCartCount() {
-    let navCartValid = localStorage.getItem('totalCountCart') || 0;
+   // Оновлення navCartCount на основі значення в localStorage
+   function updateNavCartCount() {
+      let navCartValid = localStorage.getItem('totalCountCart') || 0;
 
-    if (+navCartValid > 0) {
-      navCartCount.style.display = 'flex';
-      navCartCount.textContent = localStorage.getItem('totalCountCart');
-    } else {
-      navCartCount.style.display = 'none';
-    }
-  }
+      if (+navCartValid > 0) {
+         navCartCount.style.display = 'flex';
+         navCartCount.textContent = localStorage.getItem('totalCountCart');
+      } else {
+         navCartCount.style.display = 'none';
+      }
+   }
 
-  // Спробуйте оновити на початку для початкового стану
-  updateNavCartCount();
+   // Спробуйте оновити на початку для початкового стану
+   updateNavCartCount();
 
-  /** Count Icon Header */
+   /** Count Icon Header */
 
-  // Додайте подію 'storage', яка буде викликатися при зміні localStorage
-  window.addEventListener('storage', updateNavCartCount);
+   // Додайте подію 'storage', яка буде викликатися при зміні localStorage
+   window.addEventListener('storage', updateNavCartCount);
 
-  function isElementVisible(element) {
-    return window.getComputedStyle(element).display !== 'none';
-  }
+   function isElementVisible(element) {
+      return window.getComputedStyle(element).display !== 'none';
+   }
 
-  const accountDropdownWrapper = document.getElementById(
-    'accountDropdownWrapper'
-  );
-  const accountToggle = document.getElementById('accountToggle');
-  const logoutButton = document.getElementById('logout');
+   const accountDropdownWrapper = document.getElementById(
+      'accountDropdownWrapper'
+   );
+   const accountToggle = document.getElementById('accountToggle');
+   const logoutButton = document.getElementById('logout');
 
-  accountToggle.addEventListener('click', function (event) {
-    event.preventDefault();
-    accountDropdownWrapper.classList.toggle('active');
-  });
+   accountToggle.addEventListener('click', function (event) {
+      event.preventDefault();
+      accountDropdownWrapper.classList.toggle('active');
+   });
 
-  document.addEventListener('click', function (event) {
-    if (!event.target.closest('#accountDropdownWrapper')) {
-      accountDropdownWrapper.classList.remove('active');
-    }
-  });
+   document.addEventListener('click', function (event) {
+      if (!event.target.closest('#accountDropdownWrapper')) {
+         accountDropdownWrapper.classList.remove('active');
+      }
+   });
 });
 
 // фун для підчеркування сторінок
 (function () {
-  const hrefPags = location.href;
-  const menuLinks = document.querySelectorAll('.decstop-nav li');
+   const hrefPags = location.href;
+   console.log('✌️hrefPags --->', hrefPags);
+   const menuLinks = document.querySelectorAll('.decstop-nav li');
+   console.log('✌️menuLinks --->', menuLinks);
 
-  const header = document.querySelector('header');
+   const header = document.querySelector('header');
+   console.log('✌️header --->', header);
 
-  // Об'єкт для визначення відповідних індексів елементів меню
-  const pageMapping = {
-    index: -1, // Вказуємо -1 для сторінки "index", щоб не встановлювати клас
-    shop: 0,
-    product: 0,
-    blog: 1,
-    our: 2,
-    cart: 5,
-    checkout: 5,
-    confirmation: 5,
-    account: 6,
-  };
+   // Об'єкт для визначення відповідних індексів елементів меню
+   const pageMapping = {
+      index: '', // Вказуємо -1 для сторінки "index", щоб не встановлювати клас
+      shop: 0,
+      product: 0,
+      blog: 1,
+      our: 2,
+      cart: 5,
+      checkout: 5,
+      confirmation: 5,
+      account: 6,
+   };
 
-  // Перевірка чи частину URL містить ключі з pageMapping
-  switch (true) {
-    case hrefPags.includes('index'):
-      header.style.borderBottom = 'none';
-      break;
-    case Object.keys(pageMapping).some((key) => hrefPags.includes(key)):
-      header.style.borderBottom = '1.5px solid #D8D8D8';
-      const currentPage = Object.keys(pageMapping).find((key) =>
-        hrefPags.includes(key)
-      );
-      const typeIndex = pageMapping[currentPage];
-      if (typeIndex >= 0) {
-        const type = menuLinks[typeIndex];
-        type.classList.add('active-header-li');
-      }
-      break;
-    default:
-      console.error('Такої сторінки немає');
-  }
+   // Перевірка чи частину URL містить ключі з pageMapping
+   switch (true) {
+      case hrefPags.includes('index'):
+         header.style.borderBottom = 'none';
+         break;
+      case Object.keys(pageMapping).some((key) => hrefPags.includes(key)):
+         header.style.borderBottom = '1.5px solid #D8D8D8';
+         const currentPage = Object.keys(pageMapping).find((key) =>
+            hrefPags.includes(key)
+         );
+         console.log('✌️currentPage --->', currentPage);
+         const typeIndex = pageMapping[currentPage];
+         console.log('✌️typeIndex --->', typeIndex);
+         if (typeIndex >= 0) {
+            const type = menuLinks[typeIndex];
+            console.log('✌️type --->', type);
+            type.classList.add('active-header-li');
+         }
+         break;
+      default:
+         console.error('Такої сторінки немає');
+   }
 })();
