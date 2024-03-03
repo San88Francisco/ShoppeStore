9b2ac1848677ec3895a3c7471ac4d5998040effe
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2024-03-03 09:46:01 +0100

    deBug

--- src/assets/js/Home/Header.js
+++ src/assets/js/Home/Header.js
@@ -43,45 +43,46 @@ document.addEventListener('DOMContentLoaded', function () {
          accountDropdownWrapper.classList.remove('active');
       }
    });
+
+   // фун для підчеркування сторінок
+   (function () {
+      const hrefPags = location.href;
+      const menuLinks = document.querySelectorAll('.decstop-nav li');
+
+      const header = document.querySelector('header');
+
+      // Об'єкт для визначення відповідних індексів елементів меню
+      const pageMapping = {
+         index: '', // Вказуємо -1 для сторінки "index", щоб не встановлювати клас
+         shop: 0,
+         product: 0,
+         blog: 1,
+         our: 2,
+         cart: 5,
+         checkout: 5,
+         confirmation: 5,
+         account: 7,
+      };
+
+      // Перевірка чи частину URL містить ключі з pageMapping
+      switch (true) {
+         case hrefPags.includes('index'):
+            header.style.borderBottom = 'none';
+            break;
+         case Object.keys(pageMapping).some((key) => hrefPags.includes(key)):
+            header.style.borderBottom = '1.5px solid #D8D8D8';
+            const currentPage = Object.keys(pageMapping).find((key) =>
+               hrefPags.includes(key)
+            );
+            const typeIndex = pageMapping[currentPage];
+            if (typeIndex >= 0) {
+               const type = menuLinks[typeIndex];
+               type.classList.add('active-header-li');
+            }
+            break;
+         default:
+            console.error('Такої сторінки немає');
+      }
+   })();
 });
 
-// фун для підчеркування сторінок
-(function () {
-   const hrefPags = location.href;
-   const menuLinks = document.querySelectorAll('.decstop-nav li');
-
-   const header = document.querySelector('header');
-
-   // Об'єкт для визначення відповідних індексів елементів меню
-   const pageMapping = {
-      index: '', // Вказуємо -1 для сторінки "index", щоб не встановлювати клас
-      shop: 0,
-      product: 0,
-      blog: 1,
-      our: 2,
-      cart: 5,
-      checkout: 5,
-      confirmation: 5,
-      account: 7,
-   };
-
-   // Перевірка чи частину URL містить ключі з pageMapping
-   switch (true) {
-      case hrefPags.includes('index'):
-         header.style.borderBottom = 'none';
-         break;
-      case Object.keys(pageMapping).some((key) => hrefPags.includes(key)):
-         header.style.borderBottom = '1.5px solid #D8D8D8';
-         const currentPage = Object.keys(pageMapping).find((key) =>
-            hrefPags.includes(key)
-         );
-         const typeIndex = pageMapping[currentPage];
-         if (typeIndex >= 0) {
-            const type = menuLinks[typeIndex];
-            type.classList.add('active-header-li');
-         }
-         break;
-      default:
-         console.error('Такої сторінки немає');
-   }
-})();
